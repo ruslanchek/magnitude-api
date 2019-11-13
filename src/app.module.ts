e32e5be34945205dc012a4932773443d3c15994e
Ruslan Shashkov <ruslanchek@gmail.com>
2019-11-13 13:16:30 +0200

    Socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from './user/user.module';
 import { EmailService } from './email/email.service';
 import { EmailModule } from './email/email.module';
 import { ServiceModule } from './service/service.module';
+import { EventsModule } from './events/events.module';
 import { typeOrmConstants } from './constants';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConstants), AuthModule, UserModule, EmailModule, ServiceModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConstants), EventsModule, AuthModule, UserModule, EmailModule, ServiceModule],
   providers: [EmailService],
 })
 export class AppModule {}
